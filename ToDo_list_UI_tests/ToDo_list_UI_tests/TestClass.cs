205ff803a101b5558aca2379f30400cc1f3c8aa5
Artem Guevskiy <Artem Guevskiy@DESKTOP-6MA6TRH>
2019-02-07 18:07:27 +0200

    no message

---
+++ ToDo_list_UI_tests/ToDo_list_UI_tests/TestClass.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ToDo_list_UI_tests
+{
+    [TestFixture]
+    public class TestClass
+    {
+        IWebDriver driver = new ChromeDriver();
+
+        [Test]
+        public void Test_001()
+        {
+            driver.Navigate().GoToUrl(new Uri(@"index.jpg", UriKind.Relative));
+            Thread.Sleep(3000);
+        }
+    }
+}
