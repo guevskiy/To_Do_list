defc541dd983fa8197a46cbe64f4724f06f7d447
Artem Guevskiy <Artem Guevskiy@DESKTOP-6MA6TRH>
2019-02-15 13:52:07 +0200

    add tests

--- ToDo_list_UI_tests/ToDo_list_UI_tests/TestClass.cs
+++ ToDo_list_UI_tests/ToDo_list_UI_tests/TestClass.cs
@@ -15,11 +15,92 @@ namespace ToDo_list_UI_tests
     {
         IWebDriver driver = new ChromeDriver();
 
-        [Test]
-        public void Test_001()
+        [OneTimeSetUp]
+        public void SetUp()
         {
-            driver.Navigate().GoToUrl(new Uri(@"index.jpg", UriKind.Relative));
-            Thread.Sleep(3000);
+            driver.Navigate().GoToUrl("https://myfirstsite2243.000webhostapp.com/To_Do_List/index.html");
+            Thread.Sleep(2000);
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            driver.Close();
+            driver.Quit();
+        }
+
+        //-----------------------------------------------
+
+
+
+        public int GetCountOfitems()
+        {
+            return driver.FindElements(By.CssSelector(".todo__item")).Count;
+        }
+
+        public void Add_item(string text)
+        {
+            driver.FindElement(By.CssSelector("#todo-add-new")).SendKeys(text);
+            driver.FindElement(By.CssSelector("#btn-submit")).Click();
+        }
+
+        public void Del_items()
+        {
+            driver.FindElement(By.CssSelector("#clear-todos")).Click();
+        }
+
+        public void Del_marked_items()
+        {
+            driver.FindElement(By.CssSelector("#btn-new")).Click();
+        }
+
+        public void MarkEvenItems()
+        {
+            var elems = driver.FindElements(By.CssSelector(".todo__item"));
+            Console.WriteLine(elems.Count);
+            for (int i = 0; i < elems.Count; i =i + 2)
+            {
+                elems[i].Click();
+                Thread.Sleep(500);
+            }
+        }
+
+        public void Create_items(int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Add_item("12" + i);
+                Thread.Sleep(500);
+            }
+        }
+
+        [Test]
+        [TestCase(3)]
+        [TestCase(5)]
+        [TestCase(7)]
+        public void Test_001(int exp)
+        {
+            Create_items(exp);
+
+            Assert.AreEqual(exp, GetCountOfitems());
+            Thread.Sleep(2000);
+            Del_items();
+        }
+
+
+        [Test]
+        public void Test_002()
+        {
+            Create_items(8);
+            MarkEvenItems();
+
+            Del_marked_items();
+            Thread.Sleep(2000);
+
+            Assert.AreEqual(4, GetCountOfitems());
+            
+            Del_items();
+            Thread.Sleep(2000);
         }
     }
 }
