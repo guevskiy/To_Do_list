066e712841186328c505282129fd1682ce275148
Artem Guevskiy <Artem Guevskiy@DESKTOP-6MA6TRH>
2019-03-26 19:11:26 +0200

    no message

--- ToDo_list_UI_tests/ToDo_list_UI_tests/Tests_Request.cs
+++ ToDo_list_UI_tests/ToDo_list_UI_tests/Tests_Request.cs
@@ -161,6 +161,40 @@ namespace ToDo_list_UI_tests
             }
         }
 
+        [Test]
+        public void Test_Parse_XML_example()
+        {
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load(@"users.xml");
+            // получим корневой элемент
+            XmlElement xRoot = xDoc.DocumentElement;
+            // обход всех узлов в корневом элементе
+            foreach (XmlNode xnode in xRoot)
+            {
+                // получаем атрибут name
+                if (xnode.Attributes.Count > 0)
+                {
+                    XmlNode attr = xnode.Attributes.GetNamedItem("name");
+                    if (attr != null)
+                        Console.WriteLine(attr.Value);
+                }
+                // обходим все дочерние узлы элемента user
+                foreach (XmlNode childnode in xnode.ChildNodes)
+                {
+                    // если узел - company
+                    if (childnode.Name == "company")
+                    {
+                        Console.WriteLine("Компания: {0}", childnode.InnerText);
+                    }
+                    // если узел age
+                    if (childnode.Name == "age")
+                    {
+                        Console.WriteLine("Возраст: {0}", childnode.InnerText);
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
 
 
     }
